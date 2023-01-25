a9aeb7f3e496918894f16e3b95aff87a93eb8940
Kenny477 <53405834+Kenny477@users.noreply.github.com>
2023-01-25 12:13:19 -0800

    Scale size and change font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Work Sans', 'Inter', 'sans-serif'],
+    },
   },
   plugins: [],
 }
